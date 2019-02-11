3a0f2bf38b2fd62720d93f3abec10b03ac00ca2e
Zalaman <dcdarkninja573@gmail.com>
2019-02-11 01:41:12 +0000

    Jump + speed acc added
    Added jump and speed accleration for smoothness and practicallity to jump with gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f ;
+    public float accelerationTimeAirborne = .2f;
+    public float accelerationTimeGrounded = .1f;
     float moveSpeed = 6;
-    float gravity = -20;
+    float gravity;
+
+    float jumpVelocity;
     Vector3 velocity;
+    float velocityXSmoothing;
 
 
 
@@ -16,6 +23,10 @@ public class Player : MonoBehaviour
     private void Start()
     {
         controller = GetComponent<Controller2D>();
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        print("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
     }
     private void Update()
     {
@@ -24,8 +35,13 @@ public class Player : MonoBehaviour
             velocity.y = 0;
         }
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if(Input.GetKeyDown(KeyCode.Space)&& controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
 
-        velocity.x = input.x * moveSpeed;
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
