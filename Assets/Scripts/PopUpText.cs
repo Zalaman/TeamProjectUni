814869e6abd3f062791c8ad3fc028b35e64e7908
AvaRory414142 <47221060+AvaRory414142@users.noreply.github.com>
2019-03-03 16:06:21 +0000

    Recent Upload Implementations
    puzzle + layout changes, pop up text. (not finished)

---
+++ Assets/Scripts/PopUpText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpText : MonoBehaviour
+{
+    public GameObject uiObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(uiObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+
+    public Transform blockade;
+    public Vector3 closedPosition = new Vector3(39.02f, 2.2f, 0);
+    public Vector3 openPosition = new Vector3(39.02f, 0, 0);
+
+    public float openSpeed = 1;
+
+    private bool open = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open == true)
+        {
+            blockade.position = Vector3.Lerp(blockade.position, openPosition, Time.deltaTime * openSpeed);
+
+        }
+        else
+        {
+            CloseDoor();
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entered");
+        if(other.tag == "Pushable")
+        {
+            OpenDoor();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+            
+        
+    }
+
+    public void CloseDoor()
+    {
+        open = false;
+    }
+    public void OpenDoor()
+    {
+        open = true;
+    }
+}
