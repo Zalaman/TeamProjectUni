537daeb4f344e684b8eb6a5a6d777592977fdcb9
samallitt <47221032+samallitt@users.noreply.github.com>
2019-03-18 15:56:25 +0000

    Key Inventory Changes

---
+++ Pickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Inventory inventory;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectsWithTag("Player").GetComponent<Inventory>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    //ITEM CAN BE ADDED TO INVENTORY !
+                    inventory.isFull[i] = true;
+                    break;
+                }
+            }
+        }
+    }
+}
