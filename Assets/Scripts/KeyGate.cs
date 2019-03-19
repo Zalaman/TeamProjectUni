a3e6bdb4e33a36e28ec4fe225c503e0c6f5e5616
samallitt <samuel.allitt@googlemail.com>
2019-03-19 13:37:05 +0000

    Key and gate Implemented (but not working as intended)
    Key and gate are in the level just disabled since they aren't working correctly, will fix once get back from team project meeting

---
+++ Assets/Scripts/KeyGate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyGate : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Player" && GameVariables.keyCount>0)
+        {
+            GameVariables.keyCount --;
+            Destroy (gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/KeyItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItem : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+       if (collider.gameObject.name == "Player")
+        {
+            GameVariables.keyCount += 2;
+            Destroy (gameObject);
+        }
+    }
+}
