55931d9786ea7e95dd7f726a00ffc254602394cf
Zalaman <36924120+Zalaman@users.noreply.github.com>
2019-05-11 19:51:56 +0100

    Final update

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+
+
+   private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.transform.CompareTag("Player"))
+            col.collider.transform.position = spawnPoint.position;
+        print("collision");
+    }
+}
